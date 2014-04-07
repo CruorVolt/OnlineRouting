87703f7b72afc83b0b91dcb24c7ab7783fb681e3
Formic <M14X@M14X-PC.(none)>
2014-04-06 23:46:26 -0600

    Initial djikstras

---
+++ Algorithms/Dijkstras.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once "GraphClass/Graph.php";
+
+class Dijkstras {
+
+	public static function addShortestPath(Graph $graph, 
+		Vertex $origin, Vertex $destination) {
+
+		$vertices = $graph->getVertices();
+		$edges = $graph->getEdges();
+	}
+}
+
+?>
